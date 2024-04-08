ed05f23d0df0b9465b4dd6e0b014b2ffd4a421f2
freelife11 <freelife11@nate.com>
2024-04-08 11:37:03 +0900

    add project img

--- common/js/main.js
+++ common/js/main.js
@@ -17,42 +17,41 @@ function getRandomColor() {
 // 1초마다 폰트 컬러 변경
 setInterval(changeColor, 200);
 
+/* 마우스 따라다니는 프로젝트 이미지 */
+document.addEventListener('DOMContentLoaded', function() {
+    var projectBoxes = document.querySelectorAll('.project-box');
+    
+    projectBoxes.forEach(function(box) {
+        var image = box.querySelector('.image');
+        image.style.position = 'fixed'; // 이미지의 position을 fixed로 변경
+        image.style.pointerEvents = 'none';
+        image.style.opacity = '0'; // 이미지를 처음에 숨김
 
-// 마우스 따라다니는 이미지
-// 이미지를 따라다니게 하는 함수
-function followMouse(event, image) {
-    // 이벤트가 발생한 위치를 가져옵니다.
-    var mouseX = event.clientX;
-    var mouseY = event.clientY;
+        box.addEventListener('mouseenter', function() {
+            image.style.opacity = '1'; // 마우스가 상자 위에 있을 때 이미지를 나타냄
+        });
 
-    // 이미지의 너비와 높이를 가져옵니다.
-    var imageWidth = image.width;
-    var imageHeight = image.height;
-
-    // 이미지의 중심 좌표를 계산합니다.
-    var imageCenterX = mouseX - imageWidth / 2;
-    var imageCenterY = mouseY - imageHeight / 2;
-
-    // 이미지를 이동합니다.
-    image.style.position = 'absolute';
-    image.style.left = imageCenterX + 'px';
-    image.style.top = imageCenterY + 'px';
-}
-
-// 각 project-box 요소에 이벤트를 추가합니다.
-var projectBoxes = document.querySelectorAll('.project-box');
-projectBoxes.forEach(function(box) {
-    var image = box.querySelector('img');
-    box.addEventListener('mouseenter', function() {
         document.addEventListener('mousemove', function(event) {
-            followMouse(event, image);
+            var mouseX = event.clientX; // 전체 웹 브라우저 화면을 기준으로한 마우스 위치 계산
+            var mouseY = event.clientY;
+
+            var imageWidth = image.offsetWidth;
+            var imageHeight = image.offsetHeight;
+
+            // 이미지의 중심 좌표 계산
+            var imageX = mouseX - imageWidth / 2;
+            var imageY = mouseY - imageHeight / 2;
+
+            // 이미지가 화면을 벗어나지 않도록 조정
+            imageX = Math.min(Math.max(imageX, 0), window.innerWidth - imageWidth);
+            imageY = Math.min(Math.max(imageY, 0), window.innerHeight - imageHeight);
+
+            image.style.left = imageX + 'px';
+            image.style.top = imageY + 'px';
         });
-        showImage(image);
-    });
-    box.addEventListener('mouseleave', function() {
-        document.removeEventListener('mousemove', function(event) {
-            followMouse(event, image);
+
+        box.addEventListener('mouseleave', function() {
+            image.style.opacity = '0'; // 마우스가 상자를 떠날 때 이미지를 숨김
         });
-        hideImage(image);
     });
 });
