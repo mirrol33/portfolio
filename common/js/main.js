44ae64f163f0055d3631b925092186639f7407d8
freelife11 <freelife11@nate.com>
2024-04-21 00:26:49 +0900

    new mouse cursor

--- common/js/main.js
+++ common/js/main.js
@@ -163,4 +163,12 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
 
+    /* cursor */
+    let cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+        
+    })
+
 });
