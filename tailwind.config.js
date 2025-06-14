84af1c9b80c9d09a0f4c94b84c0ea728a2cde056
LMK <mirrol33@gmail.com>
2025-06-15 07:52:53 +0900

    06.15 update1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html", // 여기가 정확한지 확인
+    // 만약 index.html이 다른 폴더에 있다면 정확한 상대 경로를 지정해야 합니다.
+    // 예: './public/index.html'
+  ],
+  // ...
+};
